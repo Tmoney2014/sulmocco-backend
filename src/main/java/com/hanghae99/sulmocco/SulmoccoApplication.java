e08afb0536f5ac28e69b355662bf086c958dc5ca
Tmoney2014 <107834618+Tmoney2014@users.noreply.github.com>
2022-08-06 21:20:58 +0900

    빌드

---
+++ src/main/java/com/hanghae99/sulmocco/SulmoccoApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae99.sulmocco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SulmoccoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SulmoccoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanghae99/sulmocco/SulmoccoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae99.sulmocco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SulmoccoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
