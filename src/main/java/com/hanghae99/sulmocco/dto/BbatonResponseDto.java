4054e1276a82f98707bda4a60980c1abb18e0810
BaikSeunghan <tmdgks103@naver.com>
2022-08-16 22:53:48 +0900

    Bbaton login 이름 변경

---
+++ src/main/java/com/hanghae99/sulmocco/dto/BbatonResponseDto.java
@@ -0,0 +1,13 @@
+package com.hanghae99.sulmocco.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BbatonResponseDto {
+
+    private boolean isAdult;
+    private String userId;
+
+}
