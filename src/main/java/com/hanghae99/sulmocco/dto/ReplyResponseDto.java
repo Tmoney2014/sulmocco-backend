4c72dea7c706ef95b133249c30aac1a11459f7be
BaikSeunghan <tmdgks103@naver.com>
2022-08-16 23:01:24 +0900

    ReplyResponseDto

---
+++ src/main/java/com/hanghae99/sulmocco/dto/ReplyResponseDto.java
@@ -0,0 +1,23 @@
+package com.hanghae99.sulmocco.dto;
+
+import com.hanghae99.sulmocco.model.Reply;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class ReplyResponseDto {
+
+    private String username;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public ReplyResponseDto(Reply reply) {
+        this.username = reply.getUser().getUsername();
+        this.content = reply.getContent();
+        this.createdAt = reply.getCreatedAt();
+    }
+    
+}
