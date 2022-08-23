3205beb8eefab7c3c8ad1167f23d8113f1060b1e
Tmoney2014 <107834618+Tmoney2014@users.noreply.github.com>
2022-08-23 20:42:56 +0900

    chatservice의 메시지 보내는 IF문 수정

--- src/main/java/com/hanghae99/sulmocco/websocket/ChatService.java
+++ src/main/java/com/hanghae99/sulmocco/websocket/ChatService.java
@@ -50,9 +50,9 @@ public class ChatService {
 
             chatMessage.setMessage(chatMessage.getSender() + "님이 방에서 나갔습니다.");
             chatMessage.setSender("[알림]");
-            redisTemplate.convertAndSend(channelTopic.getTopic(), chatMessage);
-        }
 
+        }
+        redisTemplate.convertAndSend(channelTopic.getTopic(), chatMessage);
         //유튜브 URL을 제출한다면 WorkOut true(유저가 방에 진입하지 못함, 운동중)
 //        if(ChatMessage.MessageType.YOUTUBEURL.equals(chatMessage.getType())) {
 //            Room room = roomRepository.findByroomId(chatMessage.getRoomId()).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 방입니다."));
