19dcc132088572c1e737ca9e2e1a32f5309e6d9b
Tmoney2014 <107834618+Tmoney2014@users.noreply.github.com>
2022-08-29 15:57:26 +0900

    JWT 가 올바르지 않을때 리스폰스 메시지 앞단으로 보내기

--- src/main/java/com/hanghae99/sulmocco/security/jwt/HeaderTokenExtractor.java
+++ src/main/java/com/hanghae99/sulmocco/security/jwt/HeaderTokenExtractor.java
@@ -1,8 +1,14 @@
 package com.hanghae99.sulmocco.security.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.NoSuchElementException;
 
 @Component
@@ -14,15 +20,25 @@ public class HeaderTokenExtractor {
      */
     public final String HEADER_PREFIX = "Bearer ";
 
-    public String extract(String header, HttpServletRequest request) {
+    public String extract(String header, HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException {
         /*
          * - Token 값이 올바르지 않은경우 -
          * header 값이 비어있거나 또는 HEADER_PREFIX 값보다 짧은 경우
          * 이셉션을(예외)를 던져주어야 합니다.
          */
+
+
         if (header == null || header.equals("") || header.length() < HEADER_PREFIX.length()) {
             System.out.println("error request : " + request.getRequestURI());
-            throw new NoSuchElementException("올바른 JWT 정보가 아닙니다.");
+
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            objectMapper.writeValue(response.getWriter(),"올바른 JWT 값이 아닙니다.");
+//            throw new NoSuchElementException("올바른 JWT 정보가 아닙니다.");
+
         }
 
         /*
