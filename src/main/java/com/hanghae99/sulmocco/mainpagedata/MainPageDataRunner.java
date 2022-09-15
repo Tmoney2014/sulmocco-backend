3d3131e463d69f302553e27c8b5a41f79e605946
BaikSeunghan <tmdgks103@naver.com>
2022-09-15 22:35:58 +0900

    #

--- src/main/java/com/hanghae99/sulmocco/mainpagedata/MainPageDataRunner.java
+++ src/main/java/com/hanghae99/sulmocco/mainpagedata/MainPageDataRunner.java
@@ -1,22 +1,22 @@
-package com.hanghae99.sulmocco.mainpagedata;
-
-import com.hanghae99.sulmocco.model.Banner;
-import com.hanghae99.sulmocco.model.Dish;
-import com.hanghae99.sulmocco.model.Product;
-import com.hanghae99.sulmocco.model.User;
-import com.hanghae99.sulmocco.repository.BannerRepository;
-import com.hanghae99.sulmocco.repository.ProductRepository;
-import com.hanghae99.sulmocco.repository.TablesRepository;
-import com.hanghae99.sulmocco.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Random;
-
+//package com.hanghae99.sulmocco.mainpagedata;
+//
+//import com.hanghae99.sulmocco.model.Banner;
+//import com.hanghae99.sulmocco.model.Product;
+//import com.hanghae99.sulmocco.model.Tables;
+//import com.hanghae99.sulmocco.model.User;
+//import com.hanghae99.sulmocco.repository.BannerRepository;
+//import com.hanghae99.sulmocco.repository.ProductRepository;
+//import com.hanghae99.sulmocco.repository.TablesRepository;
+//import com.hanghae99.sulmocco.repository.UserRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.boot.ApplicationArguments;
+//import org.springframework.boot.ApplicationRunner;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.stereotype.Component;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.Random;
+//
 //@Component
 //@RequiredArgsConstructor
 //@Transactional
@@ -32,10 +32,10 @@ import java.util.Random;
 //
 //    @Override
 //    public void run(ApplicationArguments args) throws Exception {
-        //테스트 유저 1
+////        테스트 유저 1
 //        User testUser1 = new User("testUser", bCryptPasswordEncoder.encode("테ster!23"), "테스트 백", "술찌", "https://s3.ap-northeast-2.amazonaws.com/bucketservice/roomDefault.png");
 //        userRepository.save(testUser1);
-        //테스트 유저 2.3.4.5
+////        테스트 유저 2.3.4.5
 //        User testUser2 = new User("testUser2", bCryptPasswordEncoder.encode("2222@@@@"), "테스트2", "0", "https://s3.ap-northeast-2.amazonaws.com/bucketservice/roomDefault.png");
 //        User testUser3 = new User("testUser3", bCryptPasswordEncoder.encode("3333####"), "테스트3", "1", "https://s3.ap-northeast-2.amazonaws.com/bucketservice/roomDefault.png");
 //        User testUser4 = new User("testUser4", bCryptPasswordEncoder.encode("4444$$$$"), "테스트4", "2", "https://s3.ap-northeast-2.amazonaws.com/bucketservice/roomDefault.png");
@@ -45,16 +45,16 @@ import java.util.Random;
 //        userRepository.save(testUser3);
 //        userRepository.save(testUser4);
 //        userRepository.save(testUser5);
-
-        //테스트 데이터 생성
+//
+////        테스트 데이터 생성
 //        createData(2, testUser1);
-//        Banner banner1 = new Banner(1L, "https://s3.ap-northeast-2.amazonaws.com/bucketservice/sulmoggoBanner01.png", "https://www.instagram.com/sulmoggo/");
-//        Banner banner2 = new Banner(2L, "https://s3.ap-northeast-2.amazonaws.com/bucketservice/banner02.jpg", "https://www.ob.co.kr/hoegaarden");
-//        Banner banner3 = new Banner(3L, "https://s3.ap-northeast-2.amazonaws.com/bucketservice/banner03.jpg", "https://www.ob.co.kr/budweiser");
+//        Banner banner1 = new Banner(1L, "https://s3.ap-northeast-2.amazonaws.com/bucketservice/sulmoggoBanner01.png", "https://www.instagram.com/sulmoggo_official/");
+////        Banner banner2 = new Banner(2L, "https://s3.ap-northeast-2.amazonaws.com/bucketservice/banner02.jpg", "https://www.ob.co.kr/hoegaarden");
+////        Banner banner3 = new Banner(3L, "https://s3.ap-northeast-2.amazonaws.com/bucketservice/banner03.jpg", "https://www.ob.co.kr/budweiser");
 //        bannerRepository.save(banner1);
-//        bannerRepository.save(banner2);
-//        bannerRepository.save(banner3);
-
+////        bannerRepository.save(banner2);
+////        bannerRepository.save(banner3);
+//
 //        Product product1 = new Product(1L, "https://img.bbq.co.kr:449/uploads/bbq_d/thumbnail/BBQ_앱_썸네일(480x480)_후라이드류_황금올리브치킨_수정_out.png", "황금올리브치킨", "20,000", "https://www.bbq.co.kr/menu/menuView.asp?midx=2247&cidx=114&cname=%ED%99%A9%EC%98%AC+%EC%8B%9C%EA%B7%B8%EB%8B%88%EC%B2%98", "맥주");
 //        Product product2 = new Product(2L, "https://image.gsecretail.com/md/data/image/resize/20/02/8809355310220/8809355310220_M_500.jpg", "하남쭈꾸미(보통매운맛) 420g", "11,800", "https://www.gsfresh.com/md/product_detail?itemId=8809355310220&storId=3039&supplFirmCd=70041", "소주");
 //        Product product3 = new Product(3L, "https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/pd/22/0/1/6/3/1/7/QMtDz/4259016317_B.jpg", "밥도둑 하동녹차 명란김", "29,900", "https://www.11st.co.kr/products/4259016317", "맥주");
@@ -71,9 +71,9 @@ import java.util.Random;
 //        productRepository.save(product6);
 //        productRepository.save(product7);
 //        productRepository.save(product8);
-
+//
 //    }
-
+//
 //    private void createData(int count, User testUser1) {
 //        int leftLimit = 97; // letter 'a'
 //        int rightLimit = 122; // letter 'z'
@@ -133,7 +133,7 @@ import java.util.Random;
 ////            String thumbnailImgUrl = "https://bucketservice.s3.ap-northeast-2.amazonaws.com/homeTmate/roomdefault.png";
 //            String thumbnailImgUrl = "https://s3.ap-northeast-2.amazonaws.com/bucketservice/roomDefault.png";
 //
-//            Dish alcoholTable = new Dish(title, content, alcoholTag, freetag, thumbnailImgUrl, user);
+//            Tables alcoholTable = new Tables(title, content, alcoholTag, freetag, thumbnailImgUrl, user);
 //
 //            tablesRepository.save(alcoholTable);
 //
